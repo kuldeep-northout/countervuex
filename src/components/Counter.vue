7f1d130587204e40400516bc580ed3a89bc6f056
Kuldeep-Northout <kuldeep.nageshwar@Northout.com>
2021-09-30 13:41:51 +0530

    first Commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <h2>Counter</h2>
+  <h1>{{counter}}</h1>
+  <div class="btn-group"> </div>
+  <button class="btn-group-item" @click="increment">+</button>
+  <button class="btn-group-item"  @click="decrement">-</button>
+
+</div>
+
+</template>
+
+<script>
+import {mapGetters,mapActions} from "vuex"
+export default {
+    computed:{
+      ...mapGetters([
+        'counter'
+      ])
+    },
+    methods:{
+      ...mapActions([
+        'increment',
+        'decrement'
+      ])
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,27 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    count: 0,
   },
   mutations: {
+    increment(state) {
+      state.count += 1;
+    },
+    decrement(state) {
+      state.count -= 1;
+    },
   },
   actions: {
+    increment: ({ commit }) => {
+      commit("increment");
+    },
+    decrement: ({ commit }) => {
+      commit("decrement");
+    },
   },
-  modules: {
-  }
-})
+  getters: {
+    counter: (state) => state.count,
+  },
+  modules: {},
+});
